86b7ae0211ae0eda5227dfe91337d4740f7cb458
nikita-yfh <nikita.yfh@gmail.com>
2022-02-01 12:36:42 +0300

    Fix bug

--- src/editor/EditorApp.cpp
+++ src/editor/EditorApp.cpp
@@ -2,6 +2,8 @@
 #include "EditorMain.hpp"
 #include "b2_math.h"
 #include <wx/dirdlg.h>
+#include <wx/msgdlg.h>
+#include <wx/filename.h>
 
 IMPLEMENT_APP(EditorApp);
 
@@ -9,9 +11,15 @@ const b2Vec2 b2Vec2_zero(0.0f,0.0f);
 
 bool EditorApp::OnInit() {
 	wxString gameDir;
-	if(argc>=2)
-		gameDir=argv[1];
-	else{
+	if(argc>=2){
+		if(!wxDirExists(argv[1])){
+			wxMessageBox("Directory not found", "Error", wxICON_ERROR);
+			return false;
+		}
+		wxFileName dir = wxFileName::DirName(argv[1]);
+		dir.MakeAbsolute();
+		gameDir = dir.GetPath();
+	}else{
 		wxDirDialog dialog(nullptr, "Choose game directory", "", wxDD_DEFAULT_STYLE | wxDD_DIR_MUST_EXIST);
 		
 		if (dialog.ShowModal() == wxID_CANCEL)
